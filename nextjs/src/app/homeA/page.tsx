e883115945c75a3478966c994a05346b13ed1ddf
rafaelfsnT <rafaelf_santos08@outlook.com>
2024-11-11 21:35:59 -0300

    upp

--- nextjs/src/app/homeA/page.tsx
+++ nextjs/src/app/homeA/page.tsx
@@ -1,7 +1,25 @@
 "use client";
+import { useEffect } from "react";
 import { NavBarAdmin } from "../components/NavBarAdmin";
-
+import { useRouter } from "next/navigation";
+import { verificaTokenExpirado } from "../../services/token";
 export default function HomeA() {
+  const router = useRouter();
+  useEffect(() => {
+    let lsToken = localStorage.getItem('americanos.token');
+
+    let token: IToken | null = null;
+
+    if (typeof lsToken === 'string') {
+        token = JSON.parse(lsToken);
+    }
+
+    if (!token || verificaTokenExpirado(token.accessToken)) {
+      router.push("/login");
+    }
+}, [router]);
+
+
   return (
     <>
       <NavBarAdmin />
